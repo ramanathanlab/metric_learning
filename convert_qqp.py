a805edb7079b8f2497fca90775131d2ea56a2e91
Brian Hsu <bhsu@lambda1.cels.anl.gov>
2024-03-13 18:09:53 -0700

    changed config for contrastive

--- convert_qqp.py
+++ convert_qqp.py
@@ -12,16 +12,20 @@ from sentence_transformers import SentenceTransformer
 if __name__=="__main__":
     data=pd.read_csv('QQP_questions_400k.csv')
     data=data[['question1', 'question2', 'is_duplicate']]
-    data[['q1_embed', 'q2_embed']]=data[['question1', 'question2']]
+    data['question1'] = data['question1'].astype(str)
+    data['question2'] = data['question2'].astype(str)
+    # data[['q1_embed', 'q2_embed']]=data[['question1', 'question2']]
     data['is_duplicate']=data['is_duplicate'].apply(lambda x: np.array(x, dtype=np.float32))
     # dataset=TensorDataset(data)
     # dataloader=DataLoader(dataset, batch_size=10000)
 
-    model=SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    model=SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens')
 
-    data['q1_embed']=data['q1_embed'].apply(lambda x: model.encode(x))
-    data['q2_embed']=data['q2_embed'].apply(lambda x: model.encode(x))
-
+    # data['q1_embed']=data['q1_embed'].apply(lambda x: model.encode(x))
+    # data['q2_embed']=data['q2_embed'].apply(lambda x: model.encode(x))
+    for i, row in data.iterrows(): 
+        row['q1_embed']=model.encode(row['question1'])
+        row['q2_embed']=model.encode(row['question2'])
     # for i, row in data.iterrows(): 
     #     row['q1_embed']=model.encode(row['q1_embed'])
     #     row['q2_embed']=model.encode(row['q2_embed'])
